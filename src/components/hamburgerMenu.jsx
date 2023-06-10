38ce933e933caafbcd4fa729fd2ce1403774ae35
StevenOscar22 <stevenoscar37@gmail.com>
2023-06-10 15:31:38 +0700

    first commit

--- src/components/hamburgerMenu.jsx
+++ src/components/hamburgerMenu.jsx
@@ -9,13 +9,15 @@ const Hamburger = () => {
 
   return (
     <div className="hamburger" onClick={() => setOpen(!open)}>
-        <i className={`material-symbols-outlined right-[1rem] top-[1rem] fixed cursor-pointer ${open ? 'rounded-lg backdrop-blur-sm' : '"'}`}>{`${open ? 'menu_open' : 'menu'}`}</i>
-    <div className="dropdown-menu">
+        <i className={`material-symbols-outlined right-[1rem] top-[1rem] fixed cursor-pointer`}>{`${open ? 'menu_open' : 'menu'}`}</i>
+    <div className="dropdown-menu shadow-lg shadow-gray-600/75 rounded-lg backdrop-blur-sm">
       <ul className={`transition-all duration-[.5s] ease-in-expo ${open ? 'h-[292px]' : 'h-0'}`}>
           <li><Link to="cars" spy={true} smooth={true} offset={-50} duration={500} className="navbar-button text-xs sm:text-xs lg:text-sm">Cars</Link></li>
           <li><Link to="history" spy={true} smooth={true} offset={-50} duration={500} className="navbar-button text-xs sm:text-xs lg:text-sm">History</Link></li>
           <li><Link to="technology" spy={true} smooth={true} offset={-50} duration={500} className="navbar-button text-xs sm:text-xs lg:text-sm">Technology</Link></li>
           <li><Link to="production" spy={true} smooth={true} offset={-50} duration={500} className="navbar-button text-xs sm:text-xs lg:text-sm">Production</Link></li>
+          
+          {/* Contact Us Navigation */}
           <li><button className="text-sm sm:text-xs lg:text-sm bg-cyan-950 px-12 py-1 rounded-md text-slate-300 font-medium cursor-pointer" onClick={() => navigate('../pages/Contact.jsx')}>contact us</button></li>
       </ul>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
   theme: {
     extend: {
       transitionTimingFunction: {
-        'in-expo': 'cubic-bezier(0.175, 0.885, 0.32, 1.275)'
-      }
+        "in-expo": "cubic-bezier(0.175, 0.885, 0.32, 1.275)",
+      },
     },
   },
   plugins: [],
